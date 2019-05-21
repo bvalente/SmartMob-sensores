88eba4af82b1de7d7d556c0ce2b9003d0c3ec0c0
Bernardo Valente <bernardofvalente@tecnico.ulisboa.pt>
2019-05-21 16:20:48 +0100

    remove unused code

--- SmartMobCode/car-client/log_tools.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import logging
-
-LOG_DATA = True
-
-def init_logging(log_file):
-    if LOG_DATA == True:
-            #logging.basicConfig(filename=log_file, level=logging.DEBUG)
-        logging.basicConfig(filename=log_file, level=logging.DEBUG)
--- SmartMobCode/car-raspberry/log_tools.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import logging
-
-LOG_DATA = False
-
-def init_logging(log_file):
-    if LOG_DATA == True:
-            #logging.basicConfig(filename=log_file, level=logging.DEBUG)
-        logging.basicConfig(filename=log_file, level=logging.WARNING)
